ee9bd693fb048563c710d0c7e2f10025a1df47a1
MthoNtanzi <mthontanzi99@gmail.com>
2025-05-11 12:47:36 +0200

    Add images to the homePage and modify styles

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,9 +4,23 @@ import introVid from '../assets/videos/intro_video.mp4';
 import entranceImg from '../assets/images/entrance_img.jpg';
 
 function HomePage() {
+
+    const homeImages = [
+        `https://plus.unsplash.com/premium_photo-1661929519129-7a76946c1d38?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+        `https://plus.unsplash.com/premium_photo-1661907977530-eb64ddbfb88a?q=80&w=2121&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+        `https://images.unsplash.com/photo-1625244695851-1fc873f942bc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+        `https://plus.unsplash.com/premium_photo-1675615667752-2ccda7042e7e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+        `https://plus.unsplash.com/premium_photo-1675615667609-9470809c201c?q=80&w=2098&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+        `https://plus.unsplash.com/premium_photo-1675615949706-d241c824a3a8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+
+    ];
+
+    const chunkedImages = [homeImages.slice(0, 3), homeImages.slice(3, 6)]
+
     return(
         <div className='main-page'>
 
+            {/* Intro video */}
             <video className='introVid' autoPlay loop muted>
                 <source src={introVid} type="video/mp4" />
                 Your browser does not support the video tag.
@@ -21,6 +35,25 @@ function HomePage() {
                     {/* TODO Schedule Booking */}
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi facilisis ornare sodales. Morbi dapibus sem vulputate diam tincidunt, non malesuada nunc semper. Nam accumsan urna eget dolor euismod facilisis. Donec.</p>
                 </div>
+            </div>
+
+            {/* lobby and room images */}
+            <div className='container'>
+                {chunkedImages.map((row, rowIndex) => (
+                    <div className='row' key={rowIndex}>
+                        {row.map((src, index) => (
+                                <div class="col-lg-4 col-12 mb-4 d-flex justify-content-center" key={index}>
+                                <img
+                                    className='img-fluid rounded'
+                                    alt={`Hotel lobby ${rowIndex * 3 + index + 1}`}
+                                    height='200' width='250' src={src} />
+                                </div>
+                            ))
+                        }
+                    </div>
+                    ))
+                }
+                
             </div>
             
                 
