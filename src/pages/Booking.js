bec6942baea50fd075fbe5a617b7200144d5bda9
MthoNtanzi <mthontanzi99@gmail.com>
2025-05-16 13:56:32 +0200

    Final pushes, for today

--- src/pages/Booking.js
+++ src/pages/Booking.js
@@ -1,4 +1,36 @@
 function Booking() {
+    const roomsJson = {
+        "rooms": {
+            "room1": {
+                "roomName": "Presidential Suite",
+                "roomPrice": 5500,
+                "roomImages": [
+                        "",
+                        "",
+                        "",
+                    ]
+            },
+            "room2":{
+                "roomName": "Single Room",
+                "roomPrice": 2000,
+                "roomImages": [
+                        "https://images.pexels.com/photos/8142976/pexels-photo-8142976.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+                        "https://images.pexels.com/photos/8142972/pexels-photo-8142972.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+                        "https://images.pexels.com/photos/8142977/pexels-photo-8142977.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    ]
+            },
+            "room3":{
+                "roomName": "Double Room",
+                "roomPrice": 3500,
+                "roomImages": [
+                    "",
+                    "",
+                    ""
+                ]
+            }
+        }
+    }
+
     return(
         <h1>Booking Page</h1>
     )
