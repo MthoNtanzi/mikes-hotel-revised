421e0fc2471a13776b87d74ebff0229405c5eab4
MthoNtanzi <mthontanzi99@gmail.com>
2025-06-04 17:39:26 +0200

    Modify server.js

--- server/server.js
+++ server/server.js
@@ -5,6 +5,15 @@ const routes = require('./routes');
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; font-src 'self'; style-src 'self'; script-src 'self'; img-src 'self';"
+  );
+  next();
+});
+
 app.use('/api', routes);
 
 const PORT = 5000;
