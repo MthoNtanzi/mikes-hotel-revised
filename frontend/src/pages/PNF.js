2ec5620bf0266b199b8ffe4bc197def9e2d6d8ea
MthoNtanzi <mthontanzi99@gmail.com>
2025-06-04 17:12:06 +0200

    Push data

---
+++ frontend/src/pages/PNF.js
@@ -0,0 +1,24 @@
+import '../assets/styles/PNF.css';
+
+function PNF() {
+
+    
+    return(
+    <div className="container">
+      <div className="boo-wrapper">
+        <div className="boo">
+          <div className="face"></div>
+        </div>
+        <div className="shadow"></div>
+
+        <h1>Whoops!</h1>
+        <p>
+          We couldn't find the page you
+          <br />
+          were looking for.
+        </p>
+      </div>
+    </div>)
+}
+
+export default PNF;
