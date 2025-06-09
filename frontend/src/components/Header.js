1a844570750afd787b731777827d4777a4475342
MthoNtanzi <mthontanzi99@gmail.com>
2025-06-09 22:03:55 +0200

    Fix responsiveness

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,10 +3,29 @@ import React from "react";
 import '../assets/styles/Header.css';
 
 function Header() {
+
+     // Close the navbar when a link is clicked
+    const closeNavbar = () => {
+        const navbarToggler = document.querySelector('.navbar-toggler');
+        const navbarCollapse = document.querySelector('.navbar-collapse');
+        
+        // Only proceed if elements exist (mobile view)
+        if (navbarToggler && navbarCollapse) {
+            // Check if navbar is currently expanded
+            const isExpanded = navbarToggler.getAttribute('aria-expanded') === 'true';
+            
+            if (isExpanded) {
+                // Close the navbar
+                navbarToggler.setAttribute('aria-expanded', 'false');
+                navbarCollapse.classList.remove('show');
+            }
+        }
+    };
+
     return (
         <header className='navbar navbar-expand-lg navbar-light bg-light'>
             <div className="container-fluid">
-                <Link to="/" className='navbar-brand'>Home</Link>
+                <Link to="/" className='navbar-brand' onClick={closeNavbar}>Home</Link>
                 <button 
                     className="navbar-toggler" 
                     type="button" 
@@ -21,13 +40,13 @@ function Header() {
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item">
-                            <Link className='nav-link' to="/rooms">Rooms</Link>
+                            <Link className='nav-link' to="/rooms" onClick={closeNavbar}>Rooms</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className='nav-link' to="/experiences">Experiences</Link>
+                            <Link className='nav-link' to="/experiences" onClick={closeNavbar}>Experiences</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className='nav-link' to="/reservation">View Booking</Link>
+                            <Link className='nav-link' to="/reservation" onClick={closeNavbar}>View Booking</Link>
                         </li>
                     </ul>
                 </div>
