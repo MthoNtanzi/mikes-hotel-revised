c35f416a7b28f977a1b0cb964432485402bbcf9e
MthoNtanzi <mthontanzi99@gmail.com>
2025-11-10 11:09:23 +0200

    Import routes into server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import pkg from 'pg';
+import routes from './routes.js';
 const { Pool } = pkg;
 
 dotenv.config();
